fd382d797221831d03a9b4c714492c569de0ed3d
Arun <98469055+Arunpandian2306@users.noreply.github.com>
2025-02-18 18:35:52 +0530

    Update Product.js

--- routes/Controller/Product.js
+++ routes/Controller/Product.js
@@ -3,9 +3,9 @@ import {
   createProduct,
   updateProduct,
   deleteProduct,
-  listProducts,
+  listProducts,getProducts,
 } from "../Services/ProductService.js";
-import { authorizeAdmin } from "../Middleware/authenticateJWT.js";
+import { authorizeAdmin, authenticateUser } from "../Middleware/authenticateJWT.js";
 import upload from "../Middleware/uploadMiddleware.js";
 
 const router = express.Router();
@@ -17,5 +17,7 @@ router.put("/:id", authorizeAdmin, upload.single("image"), updateProduct);
 router.delete("/:id", authorizeAdmin, deleteProduct);
 
 router.get("/list", authorizeAdmin, listProducts);
+
+router.get("/getProducts", authenticateUser, getProducts);
 
 export default router;
