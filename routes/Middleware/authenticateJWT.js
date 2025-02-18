d58dd8c197db06318e3067b7545e1398dcd8aa36
Arun <98469055+Arunpandian2306@users.noreply.github.com>
2025-02-18 18:32:39 +0530

    Update authenticateJWT.js

--- routes/Middleware/authenticateJWT.js
+++ routes/Middleware/authenticateJWT.js
@@ -2,6 +2,7 @@ import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
 
 dotenv.config();
+
 export const authenticateUser = (req, res, next) => {
   const token = req.headers.authorization?.split(" ")[1];
 
@@ -10,6 +11,11 @@ export const authenticateUser = (req, res, next) => {
   }
 
   try {
+    // const decoded = jwt.decode(token);
+
+    // if (decoded?.role !== "customer") {
+    //   return res.status(403).json({ error: "Access denied. Admins only." });
+    // }
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded;
     next();
@@ -17,9 +23,22 @@ export const authenticateUser = (req, res, next) => {
     return res.status(403).json({ error: "Invalid or expired token" });
   }
 };
+
 export const authorizeAdmin = (req, res, next) => {
-  if (res.user.role !== "admin") {
-    return res.status(403).json({ error: "Access denied. Admins only." });
+  const token = req.headers.authorization?.split(" ")[1];
+
+  if (!token) {
+    return res.status(401).json({ error: "Access denied. No token provided." });
   }
-  next();
+
+  try {
+    const decoded = jwt.decode(token);
+
+    if (decoded?.role !== "admin") {
+      return res.status(403).json({ error: "Access denied. Admins only." });
+    }
+    next();
+  } catch (error) {
+    return res.status(403).json({ error: "Invalid token" });
+  }
 };
