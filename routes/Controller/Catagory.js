94c64b9f27859ae824f855f1149021cac3793062
Arun <98469055+Arunpandian2306@users.noreply.github.com>
2025-02-18 18:35:28 +0530

    Update Catagory.js

--- routes/Controller/Catagory.js
+++ routes/Controller/Catagory.js
@@ -1,17 +1,17 @@
 import express from "express";
-import { authenticateJWT, isAdmin } from "../middleware/authMiddleware";
+import { authorizeAdmin } from "../Middleware/authenticateJWT.js";
 import {
   createCategory,
   updateCategory,
   deleteCategory,
   listCategories,
-} from "../controllers/categoryController";
+} from "../Services/CatagoryService.js";
 
 const router = express.Router();
 
-router.post("/", authenticateJWT, isAdmin, createCategory);
-router.put("/:id", authenticateJWT, isAdmin, updateCategory);
-router.delete("/:id", authenticateJWT, isAdmin, deleteCategory);
-router.get("/", authenticateJWT, isAdmin, listCategories);
+router.post("/create", authorizeAdmin, createCategory);
+router.put("/:id", authorizeAdmin, updateCategory);
+router.delete("/:id", authorizeAdmin, deleteCategory);
+router.get("/", authorizeAdmin, listCategories);
 
 export default router;
