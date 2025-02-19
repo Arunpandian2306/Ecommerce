ce6868fc7013963a7f0834694d8d1c202b860c88
Arun <98469055+Arunpandian2306@users.noreply.github.com>
2025-02-19 14:37:30 +0530

    Update Cart.js

--- routes/Controller/Cart.js
+++ routes/Controller/Cart.js
@@ -1,11 +1,16 @@
 import express from "express";
-import { authorizeAdmin } from "../Middleware/authenticateJWT.js";
-import { createCart, deleteCart, viewCart } from "../Services/CartService.js";
+import { authenticateUser } from "../Middleware/authenticateJWT.js";
+import {
+  createCart,
+  deleteCart,
+  viewCart,
+  updateCartQuantity,
+} from "../Services/CartService.js";
 
 const router = express.Router();
 
-router.post("/create", authorizeAdmin, createCart);
-router.delete("/:cartId", authorizeAdmin, deleteCart);
-router.get("/:userId", authorizeAdmin, viewCart);
-
+router.post("/create", authenticateUser, createCart);
+router.delete("/:cartId", authenticateUser, deleteCart);
+router.get("/:userId", authenticateUser, viewCart);
+router.put("/update", authenticateUser, updateCartQuantity);
 export default router;
